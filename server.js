d75d4f16be8f0260506a0445d26bcba60ff0c979
unknown <mlmbufpi@gmail.com>
2020-09-30 17:54:46 -0300

    Extraindo as rotas

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+
+//arquivo de rotas
+const bootcamps = require('./routes/bootcamps')
+
 //carregando config
-dotenv.config({ path: './config/config.env' });
+dotenv.config({
+    path: './config/config.env'
+});
+
+
 
 const app = express();
+
+//montar rotas
+app.use(`/api/v1/bootcamps`, bootcamps)
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('Ouvindo a porta ', PORT);
+    console.log('Ouvindo a porta ', PORT);
 });
