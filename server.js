4ac058909cf84d16dccbf675957eca684545164f
unknown <mlmbufpi@gmail.com>
2020-10-02 10:47:59 -0300

    Add middleware

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 
 //arquivo de rotas
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require('./routes/bootcamps');
 
 //carregando config
 dotenv.config({
-    path: './config/config.env'
+  path: './config/config.env',
 });
-
-
 
 const app = express();
 
-//montar rotas
-app.use(`/api/v1/bootcamps`, bootcamps)
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
+//montar rotas
+app.use(`/api/v1/bootcamps`, bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('Ouvindo a porta ', PORT);
+  console.log('Ouvindo a porta ', PORT);
 });
