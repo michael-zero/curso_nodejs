985168b54e414a581a13bb011009f118c6265ff6
unknown <mlmbufpi@gmail.com>
2020-10-02 12:05:51 -0300

    Mongo connect

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db')
 
-//arquivo de rotas
-const bootcamps = require('./routes/bootcamps');
+
 
 //carregando config
 dotenv.config({
-  path: './config/config.env',
+    path: './config/config.env'
 });
+
+//conectando com o banco
+
+connectDB()
+
+//arquivo de rotas
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 
 //montar rotas
@@ -22,6 +29,13 @@ app.use(`/api/v1/bootcamps`, bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log('Ouvindo a porta ', PORT);
+const server = app.listen(PORT, () => {
+    console.log('Ouvindo a porta ', PORT);
 });
+
+//verificar erros - promise rejections 
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    server.close(() => process.exit(1))
+})
